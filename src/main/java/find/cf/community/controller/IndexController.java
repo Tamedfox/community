ec9f955074ed965122d6e423ae714701c4266b0b
Tamedfox <191089192@qq.com>
2019-08-21 17:07:29 +0800

    add index html

--- src/main/java/find/cf/community/controller/IndexController.java
+++ src/main/java/find/cf/community/controller/IndexController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @RequestMapping("/")
+    public String idnex(){
+        return "index";
     }
 
 }
