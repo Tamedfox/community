d2b003a5249071e36dcbde688f4d85407c03d6ec
Tamedfox <191089192@qq.com>
2019-08-21 15:38:14 +0800

    init repose

---
+++ src/main/java/find/cf/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package find.cf.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
