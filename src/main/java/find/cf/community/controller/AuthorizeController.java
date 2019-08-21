24f86e574034957f63764406fce4f8a295b54f18
Tamedfox <191089192@qq.com>
2019-08-21 19:57:47 +0800

    config to properties

---
+++ src/main/java/find/cf/community/controller/AuthorizeController.java
@@ -0,0 +1,48 @@
+package find.cf.community.controller;
+
+import find.cf.community.DTO.AccessTokenDTO;
+import find.cf.community.DTO.GitHubUser;
+import find.cf.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Clock;
+
+/**
+ * 登录git验证
+ */
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+        @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        accessTokenDTO.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GitHubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        System.out.println(user.getId());
+        return "index";
+    }
+}
